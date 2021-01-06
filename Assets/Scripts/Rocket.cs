f9fa225f0f6f5a088d3178ce48266001f1bc8ad9
Akash Tambe <akashtambe000@gmail.com>
2021-01-06 16:17:33 +0530

    rocket movement added

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    Rigidbody rocketBody;
+    AudioSource rocketAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rocketBody = this.gameObject.GetComponent<Rigidbody>();
+        rocketAudio = this.gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        processInput();
+    }
+
+    public void processInput()
+    {
+        Thrust();
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.gameObject.transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            this.gameObject.transform.Rotate(Vector3.back);
+        }
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W))
+        {
+            rocketBody.AddRelativeForce(Vector3.up);
+            if (!rocketAudio.isPlaying)
+            {
+                rocketAudio.Play();
+            }
+        }
+        else
+        {
+            if (rocketAudio.isPlaying)
+            {
+                rocketAudio.Stop();
+            }
+        }
+    }
+}
